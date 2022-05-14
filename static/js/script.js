97b7b5f89e069593069b72db4c1782283b5092ad
Bruno Melo <demelobr10@gmail.com>
2022-05-14 17:36:13 -0300

    seção de serviços.

--- static/js/script.js
+++ static/js/script.js
@@ -8,10 +8,22 @@
     window.addEventListener('scroll', function () {
         if (window.scrollY > 40){
             menu.style.backgroundColor = "#702626";
-            page_up.style.fontSize = "40px";
+            page_up.style.fontSize = "45px";
         }else{
             menu.style.backgroundColor = "";
             page_up.style.fontSize = "0px";
         }
     });
 })();
+
+/* =====================================================================
+    Navbar
+   ===================================================================== */
+
+const toggleMenu = document.querySelector('.toggle')
+const navigation = document.querySelector('.navigation');
+
+toggleMenu.onclick = function(){
+    toggleMenu.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
