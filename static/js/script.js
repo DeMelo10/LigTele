017c51a54517ef6406cdbb30ca49979ed64f2045
Bruno Melo <demelobr10@gmail.com>
2022-05-24 20:02:15 -0300

    Mudança da logo com o scroll

--- static/js/script.js
+++ static/js/script.js
@@ -3,14 +3,17 @@
    ===================================================================== */
 (function () {
     var menu = document.getElementById('navbar'); // colocar em cache
+    var logo = document.querySelector("#logo");
     var page_up = document.getElementById('pageup-icon');
     
     window.addEventListener('scroll', function () {
         if (window.scrollY > 40){
             menu.style.backgroundColor = "#702626";
+            logo.setAttribute('src','../static/images/logo-black-short-329x256.png');
             page_up.style.fontSize = "45px";
         }else{
             menu.style.backgroundColor = "";
+            logo.setAttribute('src','../static/images/logo-red-short-329x256.png');
             page_up.style.fontSize = "0px";
         }
     });
